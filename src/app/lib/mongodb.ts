dfb4df5690d530c417814e72493bbe0c81a88483
Malik Ali <alicoder592@gmail.com>
2025-09-25 17:44:41 +0500

    authenticaiton done

--- src/app/lib/mongodb.ts
+++ src/app/lib/mongodb.ts
@@ -1,45 +1,45 @@
-// import mongoose from "mongoose";
+import mongoose from "mongoose";
 
-// const MONGODB_URI = process.env.DATABASE_URL;
+const MONGODB_URI = process.env.DATABASE_URL;
 
-// if (!MONGODB_URI) {
-//   throw new Error("Please add your Mongo URI to .env.local");
-// }
+if (!MONGODB_URI) {
+  throw new Error("Please add your Mongo URI to .env.local");
+}
 
-// // Define the type for the cache
-// interface MongooseCache {
-//   conn: typeof mongoose | null;
-//   promise: Promise<typeof mongoose> | null;
-// }
+// Define the type for the cache
+interface MongooseCache {
+  conn: typeof mongoose | null;
+  promise: Promise<typeof mongoose> | null;
+}
 
-// // Extend globalThis with a typed mongoose cache
-// declare global {
-//   // eslint-disable-next-line no-var
-//   var mongooseCache: MongooseCache | undefined;
-// }
+// Extend globalThis with a typed mongoose cache
+declare global {
+  // eslint-disable-next-line no-var
+  var mongooseCache: MongooseCache | undefined;
+}
 
-// const cached = globalThis.mongooseCache;
+const cached = globalThis.mongooseCache;
 
-// if (!globalThis.mongooseCache) {
-//   globalThis.mongooseCache = { conn: null, promise: null };
-// }
+if (!globalThis.mongooseCache) {
+  globalThis.mongooseCache = { conn: null, promise: null };
+}
 
 
 
-// export async function connectToDatabase(): Promise<typeof mongoose> {
-//   if (cached.conn) return cached.conn;
+export async function connectToDatabase(): Promise<typeof mongoose> {
+  if (cached.conn) return cached.conn;
 
-//   if (!cached.promise) {
-//     cached.promise = mongoose
-//       .connect(MONGODB_URI)
-//       .then((mongoose) => mongoose)
-//       .catch((err) => {
-//         cached.promise = null; // reset promise on failure
-//         console.error("MongoDB connection error:", err);
-//         throw err; // rethrow to propagate error
-//       });
-//   }
+  if (!cached.promise) {
+    cached.promise = mongoose
+      .connect(MONGODB_URI)
+      .then((mongoose) => mongoose)
+      .catch((err) => {
+        cached.promise = null; // reset promise on failure
+        console.error("MongoDB connection error:", err);
+        throw err; // rethrow to propagate error
+      });
+  }
 
-//   cached.conn = await cached.promise;
-//   return cached.conn;
-// }
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
